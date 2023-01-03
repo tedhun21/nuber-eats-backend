f36ed4766366e7fe228a9b5dc07be714a8ca9860
Jihun Song <songjihun940409@gmail.com>
2023-01-03 23:54:36 +0900

    1.4 GRAPHQL API - InputTypes & ArgumentTypes

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -5,6 +5,12 @@ export class Restaurant {
   @Field(type => String)
   name: string;
 
-  @Field(type => Boolean, { nullable: true })
-  isGood?: boolean;
+  @Field(type => Boolean)
+  isVegan?: boolean;
+
+  @Field(type => String)
+  adress: string;
+
+  @Field(type=>String)
+  ownerName: string;
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,11 +1,16 @@
-import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver(of => Restaurant)
+@Resolver((of) => Restaurant)
 export class RestaurantResolver {
-  @Query(returns => [Restaurant])
-    restaurants(@Args('veganOnly') veganOnly:boolean): Restaurant[]{
-      return [];
-    }
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+    return true;
+  }
 }
-
