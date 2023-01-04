d5ed5a253075a6800005f311ff1962447722cc9e
Jihun Song <songjihun940409@gmail.com>
2023-01-04 21:34:18 +0900

    2.2 DATABASE CONFIGURATION - Configuring ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,18 +8,19 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal:true,
-      envFilePath: process.env.NODE_ENV === "dev" ? ".dev.env": ".test.env",
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'songjihun',
-      password: '12345',
-      database: 'nuber-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
-      logging: false,
+      logging: true,
     }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
