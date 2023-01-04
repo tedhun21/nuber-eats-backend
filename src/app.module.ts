6b6dd85a46abfb568ab6f5e342626e29a156698a
Jihun Song <songjihun940409@gmail.com>
2023-01-04 20:44:03 +0900

    2.1 DATABASE CONFIGURATION - Introducing ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    RestaurantsModule,
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath: process.env.NODE_ENV === "dev" ? ".dev.env": ".test.env",
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -21,6 +25,7 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
