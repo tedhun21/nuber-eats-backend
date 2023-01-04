40f321251f5279118967d512f8880033bf250e70
Jihun Song <songjihun940409@gmail.com>
2023-01-04 16:48:15 +0900

    1.5 GRAPHQL API - Validating ArgsTypes

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -2,13 +2,13 @@ import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver((of) => Restaurant)
+@Resolver(of => Restaurant)
 export class RestaurantResolver {
-  @Query((returns) => [Restaurant])
+  @Query(returns => [Restaurant])
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     return [];
   }
-  @Mutation((returns) => Boolean)
+  @Mutation(returns => Boolean)
   createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
     console.log(createRestaurantDto);
     return true;
