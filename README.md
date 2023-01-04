2aee216fee84c47267e9fa8c1c52dcc53cf847ff
Jihun Song <songjihun940409@gmail.com>
2023-01-04 19:08:14 +0900

    2.0 DATABASE CONFIGURATION - TypeORM Setup

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Nuber Eats
 
 The Backend of Nuber Eats Clone
+
+---
+
+프론트엔드: ReactJS. GraphQL. Typescript. Apollo. TailwindCSS
+백엔드: NestJS, Typescript, TypeORM, GraphQL
+
+---
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,26 @@
 import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'songjihun',
+      password: '12345',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: false,
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
-    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
