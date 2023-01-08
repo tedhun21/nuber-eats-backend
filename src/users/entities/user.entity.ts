fd5dddf30d1cd222c1a57a5836fd582da74025f6
Jihun Song <songjihun940409@gmail.com>
2023-01-08 17:43:35 +0900

    4.6 USER CRUD - Hashing Passwords

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -4,8 +4,10 @@ import {
   ObjectType,
   registerEnumType,
 } from '@nestjs/graphql';
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity } from 'typeorm';
+import { InternalServerErrorException } from '@nestjs/common';
 
 enum UserRole {
   Client,
@@ -33,4 +35,13 @@ export class User extends CoreEntity {
   })
   @Field((type) => UserRole)
   role: UserRole;
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch(e){
+      throw new InternalServerErrorException()
+    }
+  }
 }
