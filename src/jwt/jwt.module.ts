9b7eba7dcddb21e94552d22f01f55fe461d35944
Jihun Song <songjihun940409@gmail.com>
2023-01-10 01:08:50 +0900

    5.3 USER AUTHENTICATION - JWT Module part Two

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,13 +1,20 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
 import { JwtService } from './jwt.service';
+import { JwtModuleOptions } from './jwt.interfaces';
 
 @Module({})
 @Global()
 export class JwtModule {
-  static forRoot(): DynamicModule {
+  static forRoot(options: JwtModuleOptions): DynamicModule {
     return {
       module: JwtModule,
-      providers: [JwtService],
+      providers: [
+        {
+          provide: 'CONFIG_OPTIONS',
+          useValue: options,
+        },
+        JwtService,
+      ],
       exports: [JwtService],
     };
   }
