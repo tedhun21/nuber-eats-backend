307820caa1ff8ae08a6a8a85a6f32aebf1b14309
Jihun Song <songjihun940409@gmail.com>
2023-01-16 23:44:48 +0900

    6.6 EMAIL VERIFICATION - Mailgun API

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,8 +1,11 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { MailModuleOptions } from './mail.interfaces';
+import { MailService } from './mail.service';
 
-@Module({})
+@Module({
+  providers: [MailService]
+})
 export class MailModule {
   static forRoot(options: MailModuleOptions): DynamicModule {
     return {
@@ -12,8 +15,9 @@ export class MailModule {
           provide: CONFIG_OPTIONS,
           useValue: options,
         },
+        MailService
       ],
-      exports: [],
+      exports: [MailService],
     };
   }
 }
