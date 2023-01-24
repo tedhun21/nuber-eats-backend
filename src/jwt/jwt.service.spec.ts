a49f390360fe2f3bc4e37e988a1fa13f4723baa8
Jihun Song <songjihun940409@gmail.com>
2023-01-24 23:36:14 +0900

    8.1 UNIT TESTING JWT MAIL - JWT Sign Test

---
+++ src/jwt/jwt.service.spec.ts
@@ -0,0 +1,45 @@
+import { Test } from '@nestjs/testing';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { JwtService } from './jwt.service';
+import * as jwt from 'jsonwebtoken';
+
+jest.mock('jsonwebtoken', () => {
+  return { sign: jest.fn(() => 'TOKEN'),
+verify:jest.fn(()=>) };
+});
+
+const TEST_KEY = 'testKey';
+
+describe('JwtService', () => {
+  let service: JwtService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        JwtService,
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: { privateKey: TEST_KEY },
+        },
+      ],
+    }).compile();
+    service = module.get<JwtService>(JwtService);
+  });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+  describe('sign', () => {
+    it('should return a signed token', () => {
+      const ID = 1;
+      const token = service.sign(ID);
+      expect(typeof token).toBe('string')
+      expect(jwt.sign).toHaveBeenCalledTimes(ID);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: ID }, TEST_KEY);
+    });
+  });
+  describe('verify', ()=> {
+    it('should return the decoded token',() =>{
+
+    })
+  })
+});
--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,16 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
-
 import { JwtModuleOptions } from './jwt.interfaces';
 
 @Injectable()
 export class JwtService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
-  ) {
-    console.log(options)
-  }
+  ) {}
 
   sign(userId: number): string {
     return jwt.sign({ id: userId }, this.options.privateKey);
