a574003f25359a5e11071ac795c6738c79d095d1
Jihun Song <songjihun940409@gmail.com>
2023-02-04 20:49:27 +0900

    10.14 RESTAURANT CRUD - Pagination

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -129,16 +129,25 @@ export class RestaurantService {
     });
   }
 
-  async findCategoryBySlug({ slug }: CategoryInput): Promise<CategoryOutput> {
+  async findCategoryBySlug({
+    slug,
+    page,
+  }: CategoryInput): Promise<CategoryOutput> {
     try {
       const category = await this.categories.findOne({
         where: { slug },
-        relations: ['restaurants'],
       });
       if (!category) {
         return { ok: false, error: 'Category not found' };
       }
-      return { ok: true, category };
+      const restaurants = await this.restaurants.find({
+        where: { category: { id: category.id } },
+        take: 25,
+        skip: (page - 1) * 25,
+      });
+      category.restaurants = restaurants;
+      const totalResults = await this.countRestaurants(category);
+      return { ok: true, category, totalPages: Math.ceil(totalResults / 25) };
     } catch (error) {
       return { ok: false, error: 'Could not load category' };
     }
