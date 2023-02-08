e521f86bb1c920fbccbf595e99d6421669dd7774
Jihun Song <songjihun940409@gmail.com>
2023-02-08 22:44:18 +0900

    11.7 DISH & ORDER CRUD - Create Order part Two

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { Order } from './entities/order.entity';
 
 @Injectable()
@@ -8,5 +11,27 @@ export class OrderService {
   constructor(
     @InjectRepository(Order)
     private readonly orders: Repository<Order>,
+    @InjectRepository(Restaurant)
+    private readonly restaurants: Repository<Restaurant>,
   ) {}
+
+  async createOrder(
+    customer: User,
+    { restaurantId, items }: CreateOrderInput,
+  ): Promise<CreateOrderOutput> {
+    try {
+      const restaurant = await this.restaurants.findOne({
+        where: { id: restaurantId },
+      });
+      if (!restaurant) {
+        return {
+          ok: false,
+          error: 'Restaurant not found',
+        };
+      }
+      return { ok: true };
+    } catch (error) {
+      return { ok: false };
+    }
+  }
 }
