c3bc3a4e40b8a64e89ea1e1eb904d3b2242a6440
Jihun Song <songjihun940409@gmail.com>
2023-02-10 17:09:41 +0900

    11.12 DISH & ORDER CRUD - getOrders part One

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,20 +1,31 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { Role } from 'src/auth/role.decorator';
 import { User } from 'src/users/entities/user.entity';
 import { OrderService } from './orders.service';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
+import { Order } from './entities/order.entity';
 
-@Resolver()
+@Resolver((of) => Order)
 export class OrderResolver {
   constructor(private readonly orderService: OrderService) {}
 
-  @Mutation((type) => CreateOrderOutput)
+  @Mutation((returns) => CreateOrderOutput)
   @Role(['Client'])
   async createOrder(
     @AuthUser() customer: User,
     @Args('input') createOrderInput: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
     return this.orderService.createOrder(customer, createOrderInput);
+  }
+
+  @Query((returns) => GetOrdersOutput)
+  @Role(['Any'])
+  async getOrders(
+    @AuthUser() user: User,
+    @Args('input') getOrdersInput: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    return this.orderService.getOrders(user, getOrdersInput);
   }
 }
