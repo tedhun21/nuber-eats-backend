9c7fd28d451b4aaeb13aac28e94497f7472752ab
Jihun Song <songjihun940409@gmail.com>
2023-02-16 16:32:00 +0900

    13.2 PAYMENTS - CreatePayment part Two

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -1,15 +1,16 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Column, Entity, ManyToOne, RelationId } from 'typeorm';
 
+@InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
 export class Payment extends CoreEntity {
-  @Field((type) => Int)
+  @Field((type) => String)
   @Column()
-  transactionId: number;
+  transactionId: string;
 
   @Field((type) => User)
   @ManyToOne((type) => User, (user) => user.payments)
--- src/payments/payment.resolver.ts
+++ src/payments/payment.resolver.ts
@@ -1,5 +1,11 @@
-import { Resolver } from '@nestjs/graphql';
-import { CreatePaymentOutput } from './dtos/create-payment.dto';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { Role } from 'src/auth/role.decorator';
+import { User } from 'src/users/entities/user.entity';
+import {
+  CreatePaymentInput,
+  CreatePaymentOutput,
+} from './dtos/create-payment.dto';
 import { Payment } from './entities/payment.entity';
 import { PaymentService } from './payment.service';
 
@@ -7,6 +13,12 @@ import { PaymentService } from './payment.service';
 export class PaymentResolver {
   constructor(private readonly paymentService: PaymentService) {}
 
-
-  @Mutation(returns=>CreatePaymentOutput)
+  @Mutation((returns) => CreatePaymentOutput)
+  @Role(['Owner'])
+  createPayment(
+    @AuthUser() owner: User,
+    @Args('input') createPaymentInput: CreatePaymentInput,
+  ) {
+    return this.paymentService.createPayment(owner, createPaymentInput);
+  }
 }
