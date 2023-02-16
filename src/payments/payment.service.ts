5ae5b2116b5903552eb4e4869f0a27b2d745e9b6
Jihun Song <songjihun940409@gmail.com>
2023-02-16 16:50:48 +0900

    13.3 PAYMENTS - getPayments

--- src/payments/payment.service.ts
+++ src/payments/payment.service.ts
@@ -7,6 +7,7 @@ import {
   CreatePaymentInput,
   CreatePaymentOutput,
 } from './dtos/create-payment.dto';
+import { GetPaymentOutput } from './dtos/get-payments.dto';
 import { Payment } from './entities/payment.entity';
 
 @Injectable()
@@ -42,6 +43,19 @@ export class PaymentService {
       return { ok: true };
     } catch (error) {
       return { ok: false, error: 'Could not create payment.' };
+    }
+  }
+
+  async getPayments(user: User): Promise<GetPaymentOutput> {
+    try {
+      const payments = await this.payments.find({ where: { userId: user.id } });
+      if (!payments) {
+        return { ok: false, error: 'Could not find any payments' };
+      }
+      console.log(payments);
+      return { ok: true, payments };
+    } catch (error) {
+      return { ok: false, error: 'Could not load payments.' };
     }
   }
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -57,7 +57,7 @@ export class User extends CoreEntity {
   orders: Order[];
 
   @Field((type) => [Payment])
-  @OneToMany((type) => Payment, (payment) => payment.user)
+  @OneToMany((type) => Payment, (payment) => payment.user, { eager: true })
   payments: Payment[];
 
   @Field((type) => [Order], { nullable: true })
